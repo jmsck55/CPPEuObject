880c9206d6bf2c47b37bf43715d9a5d2ba607f59
James Cook <jmsck55@gmail.com>
2022-04-02 18:59:01 -0400

    Delete watcomdll directory

--- watcomdll/jceuobj.mk
+++
@@ -1,5 +0,0 @@
-project : C:\Users\jc410\Documents\CPPEuObject\watcomdll\jceuobj.dll C:\User&
-s\jc410\Documents\CPPEuObject\watcomdll\testdll.exe .SYMBOLIC
-
-!include C:\Users\jc410\Documents\CPPEuObject\watcomdll\jceuobj.mk1
-!include C:\Users\jc410\Documents\CPPEuObject\watcomdll\testdll.mk1
